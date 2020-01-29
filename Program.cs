ad023fbee59180f2faa8d86cd4b97a19242d9c8a
Zeeshan Abid <mail@zeeshan-abid.com>
2020-01-29 18:45:12 +0000

    program wrapped in try

--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ class Program
 {
     unsafe static void Main(string[] args)
     {
-        var instance = new Tortuga.Graphics.VulkanInstance();
-        var window = new Tortuga.Graphics.Window(
-            instance,
-            "tortuga",
-            0, 0,
-            1920, 1080,
-            Veldrid.Sdl2.SDL_WindowFlags.Resizable,
-            true);
-        var swapchain = new Tortuga.Graphics.Swapchain(instance.Devices[0], window);
-        while (window.NativeWindow.Exists)
+        try
         {
-            window.PumpEvents();
+            var instance = new Tortuga.Graphics.VulkanInstance();
+            var window = new Tortuga.Graphics.Window(
+                instance,
+                "tortuga",
+                0, 0,
+                1920, 1080,
+                Veldrid.Sdl2.SDL_WindowFlags.Resizable,
+                true);
+            var swapchain = new Tortuga.Graphics.Swapchain(instance.Devices[0], window);
+            while (window.NativeWindow.Exists)
+            {
+                window.PumpEvents();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
         }
     }
 }
