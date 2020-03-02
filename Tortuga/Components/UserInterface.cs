e10d6b6daf7a9425d283f159a80c29a24af983cf
Zeeshan Abid <mail@zeeshan-abid.com>
2020-03-02 13:57:43 +0000

    User Interface Component Updated

--- Tortuga/Components/UserInterface.cs
+++ Tortuga/Components/UserInterface.cs
@@ -1,24 +1,42 @@
 using System.Threading.Tasks;
 using System.Numerics;
-using System.Drawing;
 
 namespace Tortuga.Components
 {
     public class UserInterface : Mesh
     {
+        public bool Is3D
+        {
+            get => _is3D;
+            set
+            {
+                _is3D = value;
+                int val = 0;
+                if (value)
+                    val = 1;
+                var t = this.ActiveMaterial.UpdateUniformData<int>("Is3D", val);
+                t.Wait();
+            }
+        }
+
+        private bool _is3D = true;
+
         public UserInterface()
         {
             this.ActiveMaterial = new Graphics.Material(
                 new Graphics.Shader(
                     "Assets/Shaders/UserInterface/UserInterface.vert",
                     "Assets/Shaders/UserInterface/UserInterface.frag"
-                )
+                ),
+                false
             );
+            this.ActiveMaterial.CreateUniformData<int>("Is3D");
             this.ActiveMaterial.CreateSampledImage("albedo", 1, 1);
 
             //copy data
             var task = Task.Run(async () =>
             {
+                await this.ActiveMaterial.UpdateUniformData<int>("Is3D", 1);
                 await this.ActiveMaterial.UpdateSampledImage(
                     "albedo",
                     Graphics.Image.SingleColor(System.Drawing.Color.White)
