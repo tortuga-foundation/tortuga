1d1db826c807bf9d7344e9793b921dc29e630a07
Zeeshan Abid <mail@zeeshan-abid.com>
2020-03-17 20:34:49 +0000

    namespace

--- Tortuga.Test/UserInterface.cs
+++ Tortuga.Test/UserInterface.cs
@@ -6,11 +6,11 @@ namespace Tortuga.Test
     {
         public UserInterface()
         {
-            var activityBar = new Graphics.UI.UiBlock(System.Drawing.Color.FromArgb(255, 10, 10, 10));
-            activityBar.Constraints.X = new Graphics.UI.MaxConstraint(310.0f);
-            activityBar.Constraints.Y = new Graphics.UI.RelativeConstraint(10.0f);
-            activityBar.Constraints.Width = new Graphics.UI.RelativeConstraint(300.0f);
-            activityBar.Constraints.Height = new Graphics.UI.MaxConstraint(20.0f);
+            var activityBar = new UiBlock(System.Drawing.Color.FromArgb(255, 10, 10, 10));
+            activityBar.Constraints.X = new MaxConstraint(310.0f);
+            activityBar.Constraints.Y = new RelativeConstraint(10.0f);
+            activityBar.Constraints.Width = new RelativeConstraint(300.0f);
+            activityBar.Constraints.Height = new MaxConstraint(20.0f);
             activityBar.BorderRadius = 20.0f;
         }
     }
