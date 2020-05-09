8d9c90aa9534482e6aa723ed828d9d02f991221e
Zeeshan Abid <mail@zeeshan-abid.com>
2020-05-09 15:48:22 +0100

    Thread now sleeps when exceeding max fps

--- Tortuga/Engine.cs
+++ Tortuga/Engine.cs
@@ -97,7 +97,6 @@ namespace Tortuga
             //initialize input event system
             InputSystem.Initialize();
             _audioDevice = new AudioDevice();
-            var temp = new Tortuga.Utils.BulletPhysics.BtDispatcherInfo();
         }
 
         /// <summary>
@@ -140,10 +139,18 @@ namespace Tortuga
                         foreach (var entity in _activeScene.Entities)
                             removalTasks.Add(entity.RemoveAllMarkedForRemoval());
                         Task.WaitAll(removalTasks.ToArray());
-                        while (
-                            Settings.Graphics.MaxFramesPerSecond > 0 &&
-                            stopWatch.ElapsedMilliseconds - currentTime < (1000.0f / Settings.Graphics.MaxFramesPerSecond)
-                        );
+                        if (Settings.Graphics.MaxFramesPerSecond > 0)
+                        {
+                            int waitTime = System.Convert.ToInt32(
+                                System.MathF.Round(
+                                (
+                                    1000.0f / Settings.Graphics.MaxFramesPerSecond) - 
+                                    (stopWatch.ElapsedMilliseconds - currentTime)
+                                )
+                            );
+                            if (waitTime > 0)
+                                System.Threading.Thread.Sleep(waitTime);
+                        }
                     }
                     catch (System.Exception e)
                     {
